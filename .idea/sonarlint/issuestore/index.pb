651983f75cd91905c9314a035caca1331282ada4
DVadim001 <dvadim001@gmail.com>
2024-02-28 15:29:11 +0500

    make_add_search

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -6,3 +6,11 @@
 newmain.py,0\5\05e7da11ff9385dfd080fa55f33feec51015e5fd
 6
 app.py,1\3\13cce7fd076299c81b4986166f3d822791c9490e
+B
+templates/ask.html,0\f\0fea3d218c96e10acacba60b9fd5fa741bb0d591
+D
+templates/index.html,f\6\f69b8ce2e31684ae06f1b3e84821e07e3dfddb2f
+G
+templates/question.html,e\0\e00af05cbb396c930db1d28354ba9fb7204707ac
+M
+templates/search_results.html,c\2\c222dc647616f203a28104c557b65a30507e8019
--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 
-
 app = Flask(__name__)
-
 
 # Простейшая БД для хранения вопросов и ответов
 questions = [
@@ -16,9 +14,11 @@ answers = [
 ]
 
 
+
 @app.route('/')
 def hello_world():
     return render_template('index.html', questions=questions)
+
 
 # страница с деталями вопроси и ответа
 @app.route('/question/<int:question_id>')
@@ -35,6 +35,7 @@ def question(question_id):
 @app.route('/ask', methods=['GET', 'POST'])
 def ask():
     if request.method == 'POST':
+        # Извлечение данных из формы
         title = request.form['title']
         content = request.form['content']
 
@@ -52,6 +53,12 @@ def ask():
         return render_template('ask.html')
 
 
+@app.route('/search')
+def search():
+    query = request.args.get('query', '')
+    filtered_questions = [q for q in questions if query.lower() in q['title'].lower()
+                          or query.lower() in q['content'].lower()]
+    return render_template('search_results.html', questions=filtered_questions)
 
 
 app.run(debug=True)
